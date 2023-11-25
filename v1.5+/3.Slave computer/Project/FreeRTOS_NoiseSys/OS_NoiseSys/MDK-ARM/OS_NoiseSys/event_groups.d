2716dd02f73e25edb8ca877560efe4aca3ca2b95
lantiansb <lantiansb@foxmail.com>
2023-11-25 14:12:28 +0800

    updated

---
+++ v1.5+/3.Slave computer/Project/FreeRTOS_NoiseSys/OS_NoiseSys/MDK-ARM/OS_NoiseSys/event_groups.d
@@ -0,0 +1,15 @@
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/event_groups.c
+os_noisesys\event_groups.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+os_noisesys\event_groups.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+os_noisesys\event_groups.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+os_noisesys\event_groups.o: ../Core/Inc/FreeRTOSConfig.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+os_noisesys\event_groups.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h
---
+++ v1.5+/3.Slave computer/Project/FreeRTOS_NoiseSys/OS_NoiseSys/MDK-ARM/OS_NoiseSys/heap_4.d
@@ -0,0 +1,13 @@
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+os_noisesys\heap_4.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+os_noisesys\heap_4.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+os_noisesys\heap_4.o: D:\tt_Programs_Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+os_noisesys\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+os_noisesys\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
