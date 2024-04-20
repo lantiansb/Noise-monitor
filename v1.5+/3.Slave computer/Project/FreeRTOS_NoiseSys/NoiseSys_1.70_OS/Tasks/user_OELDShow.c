439475ecf52cc9c5574173f942bc739e4451eec0
lantiansb <lantiansb@foxmail.com>
2024-04-20 13:26:50 +0800

    删除冗余代码

--- v1.5+/3.Slave computer/Project/FreeRTOS_NoiseSys/NoiseSys_1.70_OS/Tasks/user_OELDShow.c
+++ v1.5+/3.Slave computer/Project/FreeRTOS_NoiseSys/NoiseSys_1.70_OS/Tasks/user_OELDShow.c
@@ -15,18 +15,11 @@ void OLEDShow(void)
 {
     uint32_t now_tick = 0;
     static SysStatus show_status = 0;
-    // // 该函数每次开机只执行一次
-    // if (getLocationTimeFlag == 0)
-    // {
-    //     while (GPS_GetLocationTime())
-    //         ; // 通过Air780eg获取位置与时间
-    //     getLocationTimeFlag++;
-    //     GPSLocSend(); // 发送一次位置信息给上位机
-    // }
     while (1)
     {
         now_tick = osKernelGetTickCount();
         show_status = getSysStatus(); // 获取当前系统状态，执行对应的显示功能
+
         switch (show_status)
         {
         case reatimeGetNoise:
@@ -36,6 +29,10 @@ void OLEDShow(void)
                 OLED_ShowString(1, 1, latitude);
                 OLED_ShowString(2, 1, longitude);
                 OLED_ShowNum(4, 1, sysTimeStamp, 10);
+                // OLED_ShowHexNum(4, 1, txdata[5], 2);
+                // OLED_ShowHexNum(4, 3, txdata[6], 2);
+                // OLED_ShowHexNum(4, 5, txdata[7], 2);
+                // OLED_ShowHexNum(4, 7, txdata[8], 2);
             }
             else
                 OLED_ShowString(1, 1, "location...");
